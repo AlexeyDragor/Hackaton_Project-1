4c5aa902863d4ae90e4117b4d95ac3d87e69976c
bayel040 <mamatzaparovbajel@gmail.com>
2022-12-17 14:57:07 +0300

    hero

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+const changeBackground = document.querySelector('.logo__change')
+const heroChange = document.querySelector('.hero')
+
+changeBackground = addEventListener('click', () =>{
+  heroChange.style.background = 'url("https://w-dog.ru/wallpapers/10/15/311409234468799/priroda-noch-zima-sneg-rozhdestvo-gorod-ogni-novyj-god.jpg") no-repeat center/cover'
+   
+})
+
+  
+
+  
